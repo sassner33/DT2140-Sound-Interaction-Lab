b22316978ed691469ea0db06a35c16f9155bf742
William Sassner Andersson <williamsassner@outlook.com>
2025-11-19 13:31:05 +0100

    Update interaction-3.js

--- interaction-3.js
+++ interaction-3.js
@@ -53,7 +53,7 @@ wind.createDSP(audioContext, 1024)
 
 function accelerationChange(accx, accy, accz) {
     let movement = abs(accx) + abs(accy) + abs(accz);
-    if (movement > 15) {  
+    if (movement > 10) {  
         playAudio(1)
     } else {
         playAudio(0)
